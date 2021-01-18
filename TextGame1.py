682fd2b9b34a37ecf273cb881f110e33b9fbb62d
Branden Gist <breezygeazygist2121@gmail.com>
2021-01-17 21:37:52 -0500

    Add files via upload

---
+++ TextGame1.py
@@ -0,0 +1,26 @@
+answer = input("Would you like to like to accept my quest? (yes/no) ")
+
+if answer.lower().strip() == "yes":
+  answer = input("You awoken in a tavern drunk and drenched in rum and white gull. Do you go for another round? ")
+  if answer == "yes":
+    print("You grabbed another bottle of white gull and drowned yourself in the numbing pain eventaully killing yourself... Bad ending Useless Protag")
+  else:
+    answer = input("Pushed aside the white gull and drunkly stand up mumbling to yourself. The waitress comes to you well it's about time you leave you stupid drunk "
+                   "you look up. Do you unleash yellow rain or ignore her ")
+    if answer =="yellow rain":
+      print("You uzipped your pants pointing your anime protagnist cock in the air unleashing a massive load of dark yellow piss in the air " 
+            "as you drunkly laugh and stumble backwards everyone in the tavern left either impressed or disguted maybe even both... Congrats dick. Bad ending Kelly")
+    else:
+      answer = input("You know I used to be a world famous witcher until I took a witch to the sack causing my left scrotum to implode, we both stood there standing in a painfully slient stare. " 
+                     "She began to say something but I had intrupted gender equaltiy and all as she interupted my drunken mumbles of thought ")
+
+
+
+
+
+
+
+
+
+
+
